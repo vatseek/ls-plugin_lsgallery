3047f624554852f6613059566bd251409f58dbaa
vatseek <vatseek13@rambler.ru>
2012-12-18 17:08:15 +0200

    12

--- tests.sql
+++ tests.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `prefix_comment` MODIFY target_type enum('image','topic','talk') DEFAULT 'topic';
 
-SHOW CREATE TABLE `prefix_comment`;
+ALTER TABLE `prefix_favourite` MODIFY target_type enum('image', 'topic', 'comment', 'talk') DEFAULT 'topic';
+
+ALTER TABLE `prefix_vote` MODIFY target_type enum('image', 'topic', 'blog', 'user', 'comment') DEFAULT 'topic';
